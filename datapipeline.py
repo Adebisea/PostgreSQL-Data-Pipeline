28f30dc336cb6cd0796448ba7fc8e34430e46a3a
Adebisea <abdulgkhadijah@gmail.com>
2024-01-21 21:07:45 +0100

    updated the script with the load_data class initiation

--- datapipeline.py
+++ datapipeline.py
@@ -1,5 +1,28 @@
 import psycopg2
+import os
 from dotenv import load_dotenv
 load_dotenv()
+
+class Load_Data():
+
+    def __init__(self,host,port,dbname,user,password):
+
+        self.host = host
+        self.port = port
+        self.dbname = dbname
+        self.user = user
+        self.password = password
+
+
+
+
+
+if __name__ == "__main__":
+
+    host= os.getenv('host')
+    port= os.getenv('port')
+    dbname= os.getenv('dbname')
+    user= os.getenv('user')
+    password= os.getenv('password')
 
 
