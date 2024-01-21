fd6272eab3a928dc1a394224d1a4af4b484edc0f
Adebisea <abdulgkhadijah@gmail.com>
2024-01-21 21:22:13 +0100

    updated the script with db_conn, execute_queries and create_table methods

--- datapipeline.py
+++ datapipeline.py
@@ -13,9 +13,50 @@ class Load_Data():
         self.user = user
         self.password = password
 
+    def db_conn(self):
+    #Connect to the postgres db
 
+        try:
+            connect = psycopg2.connect(
+                host=self.host,
+                port=self.port,
+                dbname=self.dbname,
+                user=self.user,
+                password=self.password
+                )
+            return connect
+        except psycopg2.Error as e:
+            print('Error connecting to db:', e)
 
+    def execute_queries(self, query):
+        
+        connect = self.db_conn()
+        cursor = connect.cursor()
+        try:
+            cursor.execute(query)
+            print('Query successfully executed')
+        except psycopg2.Error as e:
+            print('Error executing query:', e)
 
+        connect.commit()
+        cursor.close()
+        connect.close()
+
+    def create_table(self):
+
+        query = '''CREATE TABLE IF NOT EXISTS onlineretail(
+                        InvoiceNo varchar(10),
+                        StockCode varchar(25),
+                        Describe varchar(50),
+                        Quantity int,
+                        InvoiceDate timestamp,
+                        UnitPrice real,
+                        CustomerID int,
+                        Country varchar(25))'''
+        
+        self.execute_queries(query)
+
+        
 
 if __name__ == "__main__":
 
