9fa0cdac07f0b66816195ae3060c4dbda539c4a7
Adebisea <abdulgkhadijah@gmail.com>
2024-01-30 16:11:17 +0100

    data retrieve pipeline

---
+++ retrievedata.py
@@ -0,0 +1,19 @@
+import pandas as pd
+from sqlalchemy_pipeline import create_sql_engine
+
+def retrieve_UKdata():
+
+ 
+    engine = create_sql_engine()
+    query = ''' 
+                select * from onlineretail where country = 'United Kingdom' LIMIT 100
+            '''    
+    df = pd.read_sql(sql=query,con=engine)
+
+    #save data from United kingdom to filegit 
+    df.to_csv('ukretail.csv', index=False)
+
+
+if __name__ == '__main__':
+
+    retrieve_UKdata()
